49bd809c9fdd70a65485376eb1ad5993f898d599
alelojett <alexis.pv19@gmail.com>
2025-05-09 19:32:22 -0600

    added the new list of containers

--- js/gtm-manager.js
+++ js/gtm-manager.js
@@ -1,11 +1,11 @@
-(function(w,d,s,l,ids){
+(function(w, d, s, l, ids) {
     w[l] = w[l] || [];
-    w[l].push({'gtm.start': new Date().getTime(), event: 'gtm.js'});
-    
+    w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+
     ids.forEach(function(id) {
         var f = d.getElementsByTagName(s)[0],
             j = d.createElement(s),
-            dl = l != 'dataLayer' ? '&l=' + l : '';
+            dl = l !== 'dataLayer' ? '&l=' + l : '';
         j.async = true;
         j.src = 'https://www.googletagmanager.com/gtm.js?id=' + id + dl;
         f.parentNode.insertBefore(j, f);
@@ -15,5 +15,5 @@
     'GTM-MZKXDKWP',
     'GTM-P4T82SZ2',
     'GTM-K2W4BZGL',
-    'GTM-5SGFTGB9' // Este que mostraste
+    'GTM-5SGFTGB9'
 ]);
