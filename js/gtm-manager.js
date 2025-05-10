136789bea15e48010fc24923f5776d37a0aeb5f5
alelojett <alexis.pv19@gmail.com>
2025-05-09 18:08:16 -0600

    simplifying the gtm scripts

--- js/gtm-manager.js
+++ js/gtm-manager.js
@@ -1,56 +1,19 @@
-// Shared dataLayer name used by all GTM containers
-window.dataLayer = window.dataLayer || [];
-window.dataLayer.push({
-    'event': 'gtm.js',
-    'gtm.start': new Date().getTime()
-});
-
-// List of GTM container IDs to load
-const gtmContainerIDs = [
-    'GTM-W9M7L2RV', // Iohan
-    'GTM-MZKXDKWP', // Omar
-    'GTM-P4T82SZ2', // Valeria
-    'GTM-K2W4BZGL'  // Alexis
-];
-
-// Function to inject GTM <script> tags into <head>
-function insertGTMScripts() {
-    const head = document.head;
-    gtmContainerIDs.forEach(id => {
-        const script = document.createElement('script');
-        script.async = true;
-        script.src = `https://www.googletagmanager.com/gtm.js?id=${id}`;
-        head.insertBefore(script, head.firstChild);
+(function(w,d,s,l,ids){
+    w[l] = w[l] || [];
+    w[l].push({'gtm.start': new Date().getTime(), event: 'gtm.js'});
+    
+    ids.forEach(function(id) {
+        var f = d.getElementsByTagName(s)[0],
+            j = d.createElement(s),
+            dl = l != 'dataLayer' ? '&l=' + l : '';
+        j.async = true;
+        j.src = 'https://www.googletagmanager.com/gtm.js?id=' + id + dl;
+        f.parentNode.insertBefore(j, f);
     });
-}
-
-// Function to inject GTM <noscript> fallback into <body>
-function insertGTMNoscripts() {
-    const body = document.body;
-    gtmContainerIDs.forEach(id => {
-        const noscript = document.createElement('noscript');
-        const iframe = document.createElement('iframe');
-        iframe.src = `https://www.googletagmanager.com/ns.html?id=${id}`;
-        iframe.height = "0";
-        iframe.width = "0";
-        iframe.style.display = "none";
-        iframe.style.visibility = "hidden";
-        noscript.appendChild(iframe);
-        body.insertBefore(noscript, body.firstChild);
-    });
-}
-
-// Wait until DOM is ready before injecting GTM
-function initializeGTM() {
-    if (document.readyState === 'loading') {
-        document.addEventListener('DOMContentLoaded', () => {
-            insertGTMScripts();
-            insertGTMNoscripts();
-        });
-    } else {
-        insertGTMScripts();
-        insertGTMNoscripts();
-    }
-}
-
-initializeGTM();
+})(window, document, 'script', 'dataLayer', [
+    'GTM-W9M7L2RV',
+    'GTM-MZKXDKWP',
+    'GTM-P4T82SZ2',
+    'GTM-K2W4BZGL',
+    'GTM-5SGFTGB9' // Este que mostraste
+]);
